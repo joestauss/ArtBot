06577ceaeaa5cc9f120784a5c59baca3127e1f7f
Joe Stauss <jstauss008@gmail.com>
2021-01-12 15:03:18 -0800

    0.2 - ContentRecord
    * Added the ContentRecord class, including initialization from a data dictionary.
    * Implemented JSON IO for ArtBots.
    * Uploaded Example_ArtBotContent.json.

--- ArtBot.py
+++ ArtBot.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import SeleniumLocator
 import time
+from pathlib import Path
+import json
 
 class SeleniumContext:
     #   SeleniumContext was taken from my other project,
@@ -35,10 +37,66 @@ class SeleniumContext:
         def __exit__(self, exc_type, exc_value, exc_traceback):
             self.driver.quit()
 
+class ContentRecord:
+    def __init__(self, subject, post_text, hashtags=[], image_file=None):
+        self.subject    = subject
+        self.post_text  = post_text
+        self.hashtags   = hashtags
+        self.image_file = image_file
+
+    def __str__(self):
+        data_dictionary = {
+            'subject'   : self.subject,
+            'post_text' : self.post_text,
+            'hashtags'  : self.hashtags,
+            'image_file': self.image_file
+        }
+        return json.dumps( data_dictionary, indent=1)
+
+    def from_data_dictionary( data_dictionary):
+        ''' Generates a ContentRecord object from a dictionary of values.
+
+        Parameters
+        ----------
+        data_dictionary: dict
+            {   "subject"   : str,
+                "post_text" : str,
+                "hashtags"  : [str],
+                "image_file": str  (for now, but a pathtype soon)
+            }
+        '''
+        return ContentRecord(
+            data_dictionary[ 'subject'],
+            data_dictionary[ 'post_text'],
+            hashtags=data_dictionary[ 'hashtags'],
+            image_file=data_dictionary[ 'image_file']
+        )
 
 class ArtBot:
     def __init__( self):
-        pass
+        self.content = []
+
+    def from_json( json_string):
+        '''Initializes an ArtBot object from json_string.
+
+        Parameters
+        -----------
+        json_string: str
+            Has the following structure:
+            {   ContentRecords: [
+                {   "subject"   : str,
+                    "post_text" : str,
+                    "hashtags"  : [str],
+                    "image_file": str    } ] }
+        '''
+        data_dictionary = json.loads( json_string)
+        artbot = ArtBot()
+        artbot.content = [ ContentRecord.from_data_dictionary( dd) for dd in data_dictionary[ 'ContentRecords'] ]
+        return artbot
+
+    @property
+    def content_as_json( self):
+        return json.dumps( {'ContentRecords' : self.content})
 
 class TwitterArtBot( ArtBot):
     def __init__( self):
@@ -69,7 +127,17 @@ class TwitterArtBot( ArtBot):
         login_button.click()
 
 if __name__=='__main__':
-    account = TaglinesOfTerror()
-    artbot  = TwitterArtBot()
-    artbot.set_login_identity( account.username, account.password)
-    artbot.text_post( "Blah blah blah, #hashtag #yayayaIAmLorde")
+    LOGIN_EXAMPLE = False
+    READING_EXAMPLE = True
+
+    if LOGIN_EXAMPLE:
+        account = TaglinesOfTerror()
+        artbot  = TwitterArtBot()
+        artbot.set_login_identity( account.username, account.password)
+        artbot.text_post( "Blah blah blah, #hashtag #yayayaIAmLorde")
+
+    if READING_EXAMPLE:
+        json_string = Path('Example_ArtBotContent.json').read_text()
+        artbot = ArtBot.from_json( json_string)
+        for record in artbot.content:
+            print( ("="*30) + str(record.post_text) +("="*30))
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 # Development History
 
+* __0.2__ : Implemented ContentRecord class; JSON import/ export.
 * __0.1__ : This project was separated from "Data Scraping and Browser Automation".
